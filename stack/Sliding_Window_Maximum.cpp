a9e8b2d38be2c6cc4d7f4da263d073619981b4e5
Rohit Kumar <32030787+rohitishu@users.noreply.github.com>
2020-06-02 01:23:49 +0530

    Sliding Window Maximum added time and space compl.

--- stack/Sliding_Window_Maximum.cpp
+++ stack/Sliding_Window_Maximum.cpp
@@ -1,3 +1,10 @@
+// for this approach we use some data structure where we store the result at front of the window every time suppose if we have a window 
+// size of 3 we can have answer for first three element at index 0 and so on and whenever we reached max size of window we will remove the 
+// front element 
+// NOTE:- we never want an element which is smaller than the current element it means whatever the element which are less than on left 
+// side of the greater element we can remove them they will never use in any window formed
+// for such application where we need both direction back and front --> back for insert new elem. and front for taking max elem in window 
+// Time complexity--> O(nk) Space complexity-->O(n)
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& v, int k) {
